723bc1f3f79d2572e33f185813b9eb3d2544b01a
MelloukOthmane <othmane.mellouk@usmba.ac.ma>
2022-10-31 13:06:40 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PrimeNumber
+{
+    class Ex
+    {
+        public static void Main(string[] args)
+        {
+            int n, i, a = 0, drp = 0;
+            Console.Write(" N = ");
+            n = int.Parse(Console.ReadLine());
+            a = n / 2;
+            for (i = 2; i <= a; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Non Premier");
+                    drp = 1;
+                    break;
+                }
+            }
+        }
+    }
+}
